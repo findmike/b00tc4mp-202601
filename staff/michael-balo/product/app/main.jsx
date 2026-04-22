f4b48dad39a0ef2116cf106402f665d51564066d
Michael Balo <mikejanoski95@gmail.com>
2026-04-22 17:44:17 +0200

    add all the profiles with jsx

--- staff/michael-balo/product/app/main.jsx
+++ staff/michael-balo/product/app/main.jsx
@@ -1,9 +1,13 @@
 import { logic } from './logic.mjs'
-import { createLandingView } from './views.jsx'
+import { createLandingView, createLoginView, createRegisterView, createHomeView, createProfileView } from './views.jsx'
 
 var rootDiv = document.getElementById('root')
 var root = ReactDOM.createRoot(rootDiv)
 
 var landingView = createLandingView()
+var loginView = createLoginView()
+var registerView = createRegisterView()
+var homeView = createHomeView()
+var profileView = createProfileView()
 
-root.render([landingView])
+root.render([landingView, loginView, registerView, homeView, profileView])
